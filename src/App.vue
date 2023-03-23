a8cff04796efa558412a5bc4d78143875e765e28
vmoltaemcrkonrgcechd <vmoltaemcrkonrgcechd@gmail.com>
2023-03-23 21:45:13 +0700

    added server connection

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,25 @@ import LineChart from "@/components/LineChart.vue";
 import BarChart from "@/components/BarChart.vue";
 import DoughnutChart from "@/components/DoughnutChart.vue";
 import RadarChart from "@/components/RadarChart.vue";
+import { ref, onMounted } from "vue";
+import { iResponse } from "@/types";
+
+const lineChartData = ref<number[][]>([]);
+const barChartData = ref<number[][]>([]);
+const doughnutChartData = ref<number[]>([]);
+const radarChartData = ref<number[][]>([]);
+
+onMounted(() => {
+  const conn = new WebSocket("ws://localhost:80");
+
+  conn.onmessage = ({ data }) => {
+    const response: iResponse = JSON.parse(data);
+    lineChartData.value = response.lineChartData;
+    doughnutChartData.value = response.doughnutChartData;
+    barChartData.value = response.barChartData;
+    radarChartData.value = response.radarChartData;
+  };
+});
 </script>
 
 <template>
@@ -11,40 +30,16 @@ import RadarChart from "@/components/RadarChart.vue";
     class="lg:h-screen grid lg:grid-cols-2 grid-rows-2 p-4 gap-2 bg-gray-100"
   >
     <div class="card">
-      <LineChart
-        :data="[
-          [10, 40, 20, 15, 5],
-          [40, 90, 15, 30, 65],
-          [25, 15, 95, 110, 70],
-          [85, 70, 75, 105, 45],
-          [55, 5, 15, 125, 20],
-        ]"
-      />
+      <LineChart :data="lineChartData" />
     </div>
     <div class="card">
-      <BarChart
-        :data="[
-          [10, 40, 20, 15, 5],
-          [40, 90, 15, 30, 65],
-          [25, 15, 95, 110, 70],
-          [85, 70, 75, 105, 45],
-          [55, 5, 15, 125, 20],
-        ]"
-      />
+      <BarChart :data="barChartData" />
     </div>
     <div class="card">
-      <DoughnutChart :data="[10, 40, 20, 15, 5]" />
+      <DoughnutChart :data="doughnutChartData" />
     </div>
     <div class="card">
-      <RadarChart
-        :data="[
-          [10, 40, 20, 15, 5],
-          [40, 90, 15, 30, 65],
-          [25, 15, 95, 110, 70],
-          [85, 70, 75, 105, 45],
-          [55, 5, 15, 125, 20],
-        ]"
-      />
+      <RadarChart :data="radarChartData" />
     </div>
   </div>
 </template>
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export interface iResponse {
+  lineChartData: number[][];
+  barChartData: number[][];
+  radarChartData: number[][];
+  doughnutChartData: number[];
+}
